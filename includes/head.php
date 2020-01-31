e82425c9bffa5ce9ba80e566f1a278ddee7e6385
Tinram <Tinram@users.noreply.github.com>
2020-01-31 20:15:37 +0000

    update includes files

--- includes/head.php
+++ includes/head.php
@@ -1,28 +1,20 @@
 <?php
+declare(strict_types=1);
 ini_set('date.timezone', TIMEZONE);
 header('Content-Type: text/html; charset=utf-8');
 ?><!DOCTYPE html>
 
 <html lang="en">
 
-	<head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php echo CONFIG_APP_NAME; ?></title>
+        <meta name="application-name" content="<?php echo CONFIG_APP_NAME; ?>">
+        <meta name="author" content="Martin Latter">
+        <meta name="copyright" content="&copy; 2015">
+        <link rel="stylesheet" type="text/css" href="css/noter.css">
+        <script src="js/common.js"></script>
+    </head>
 
-		<meta charset="utf-8">
-
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<title><?php echo CONFIG_APP_NAME; ?></title>
-
-		<meta name="application-name" content="<?php echo CONFIG_APP_NAME; ?>">
-
-		<meta name="author" content="Martin Latter">
-
-		<meta name="copyright" content="&copy; 2015">
-
-		<link rel="stylesheet" type="text/css" href="css/noter.css">
-
-		<script type="text/javascript" src="js/common.js"></script>
-
-	</head>
-
-	<body>
+    <body>
