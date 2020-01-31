21e4b31a85aa9f653ad6f60ba872eeb8ae88a677
Tinram <Tinram@users.noreply.github.com>
2020-01-31 20:16:47 +0000

    update config.php

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
-	* Configuration file of constants for noter.
-	* @author  Martin Latter
-	* @link    https://github.com/Tinram/noter.git
+    * Configuration file for Noter.
+    * @author  Martin Latter
+    * @link    https://github.com/Tinram/noter.git
 */
 
+declare(strict_types=1);
 
 ##################################################
 ## SQLITE DB
@@ -18,7 +19,7 @@ define('CONFIG_TABLE', 'notes');
 ##################################################
 ## APP
 ##################################################
-define('CONFIG_NUM_NOTES_DISPLAYED', 7);
+define('CONFIG_NUM_NOTES_DISPLAYED', 10);
 define('CONFIG_MAX_TITLE_LEN', 80);
 define('CONFIG_MAX_BODY_LEN', 8192);
 define('TIMEZONE', 'Europe/London');
@@ -29,7 +30,7 @@ define('CONFIG_APP_NAME', 'Noter');
 ##################################################
 ## ENCODING
 ##################################################
-define('CONFIG_UNICODE', TRUE);
+define('CONFIG_UNICODE', true);
 define('CONFIG_ENCODING', 'UTF-8');
 ##################################################
 
@@ -42,5 +43,3 @@ define('CONFIG_SESSION_TIMEOUT', 1800); # 30 mins
 define('CONFIG_HACK_DELAY', 3600);
 define('CONFIG_HASH', 'sha256');
 ##################################################
-
-?>
